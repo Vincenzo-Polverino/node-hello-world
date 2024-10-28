a06d51e871eb4901df62e65649658534db42c24f
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-28 16:05:12 +0100

    add condition

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 console.log('hello node js');
 console.log(process.env.PIN);
 console.log(process.argv);
+
+if (process.argv == 'admin' & PIN== 0000) {
+    console.log('Welcome Admin');
+
+}
+console.log('Access restricted');
